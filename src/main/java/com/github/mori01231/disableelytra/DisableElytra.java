22ee3fc39a540fdfebfda00486653be27179679f
Mori01231 <mori01231mc@gmail.com>
2020-08-12 22:49:53 +0900

    [Add] Initial commit

---
+++ src/main/java/com/github/mori01231/disableelytra/DisableElytra.java
@@ -0,0 +1,28 @@
+package com.github.mori01231.disableelytra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableElytra extends JavaPlugin {
+
+
+    private static DisableElytra instance;
+
+    public DisableElytra (){
+        instance = this;
+    }
+
+    public static DisableElytra getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("DisableElytra has been enabled.");
+        this.saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("DisableElytra has been disabled.");
+    }
+}
